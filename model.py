0a7d182d90a4ffd6d0f32841c0d009e859ec4250
LiuJiajun <ljj@U-2NC6P7LC-0542.local>
2026-03-01 16:29:49 +0800

    Add MultiHeadAttention class implementation

--- model.py
+++ model.py
@@ -31,7 +31,7 @@ class SelfAttention_v2(nn.Module):
         return context_vec
 
 class CausalAttention(nn.Module):
-    def __init__(self, d_in, d_out, context_length, qkv_bias=False, dropout=0.1):
+    def __init__(self, d_in, d_out, context_length, dropout=0.1, qkv_bias=False):
         super().__init__()
         self.d_out = d_out
         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
@@ -62,10 +62,69 @@ class CausalAttention(nn.Module):
         context_vec = attn_weights @ values
         return context_vec
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads,  qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), "d_out must be divisible by num_heads"
+        
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.context_length = context_length
+        
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+
+    def forward(self, x):
+        b, n, d_in = x.shape
+        
+        keys = self.W_key(x).view(b, n, self.num_heads, self.head_dim).transpose(1, 2)
+        queries = self.W_query(x).view(b, n, self.num_heads, self.head_dim).transpose(1, 2)
+        values = self.W_value(x).view(b, n, self.num_heads, self.head_dim).transpose(1, 2)
+        
+        attn_scores = queries @ keys.transpose(2, 3)
+        attn_scores = attn_scores.masked_fill(
+            self.mask[:n, :n] == 1, float("-inf")
+        )
+        
+        attn_weights = torch.softmax(
+            attn_scores / self.head_dim ** 0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights)
+        
+        context_vec = attn_weights @ values
+        context_vec = context_vec.transpose(1, 2).contiguous().view(b, n, self.d_out)
+        context_vec = self.out_proj(context_vec)
+        
+        return context_vec
+
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention(
+                d_in, d_out, context_length, dropout, qkv_bias
+             )
+             for _ in range(num_heads)]
+        )
+
+    def forward(self, x):
+        return torch.cat([head(x) for head in self.heads], dim=-1)
+
 batch = torch.stack((inputs, inputs), dim=0)
-print(batch.shape)
 torch.manual_seed(123)
-context_length = batch.shape[1]
-ca = CausalAttention(3, 2, context_length, 0.0)
-context_vecs = ca(batch)
+batch_size, context_length, d_in = batch.shape
+d_out = 2
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+context_vecs = mha(batch)
+print(context_vecs)
 print("context_vecs.shape:", context_vecs.shape)
