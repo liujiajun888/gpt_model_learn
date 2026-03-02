750a63eac8e46e4e68807081f6d2cf6d783e7bda
LiuJiajun <ljj@U-2NC6P7LC-0542.local>
2026-03-02 18:55:55 +0800

    Add DummyGPTModel, DummyTransformerBlock, DummyLayerNorm classes

--- model.py
+++ model.py
@@ -1,16 +1,7 @@
 import torch
 import torch.nn as nn
-import math
+import tiktoken
 
-
-inputs = torch.tensor(
-  [[0.43, 0.15, 0.89], # Your     (x^1)
-   [0.55, 0.87, 0.66], # journey  (x^2)
-   [0.57, 0.85, 0.64], # starts   (x^3)
-   [0.22, 0.58, 0.33], # with     (x^4)
-   [0.77, 0.25, 0.10], # one      (x^5)
-   [0.05, 0.80, 0.55]] # step     (x^6)
-)
 
 class SelfAttention_v2(nn.Module):
     def __init__(self, d_in, d_out, qkv_bias=False):
@@ -106,25 +97,116 @@ class MultiHeadAttention(nn.Module):
         
         return context_vec
 
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,     # 词汇表大小
+    "context_length": 1024,  # 上下文长度
+    "emb_dim": 768,          # 嵌入维度
+    "n_heads": 12,           # 注意力头的数量
+    "n_layers": 12,          # 层数
+    "drop_rate": 0.1,        # dropout率
+    "qkv_bias": False        # 查询-键-值偏置
+}
 
-class MultiHeadAttentionWrapper(nn.Module):
-    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
         super().__init__()
-        self.heads = nn.ModuleList(
-            [CausalAttention(
-                d_in, d_out, context_length, dropout, qkv_bias
-             )
-             for _ in range(num_heads)]
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))
+            ))
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
         )
 
     def forward(self, x):
-        return torch.cat([head(x) for head in self.heads], dim=-1)
+        return self.layers(x)
 
-batch = torch.stack((inputs, inputs), dim=0)
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.attention = MultiHeadAttention(
+            cfg["emb_dim"], 
+            cfg["emb_dim"], 
+            cfg["context_length"], 
+            cfg["drop_rate"], 
+            cfg["n_heads"],
+            cfg["qkv_bias"]
+        )
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.ff = FeedForward(cfg)
+        self.dropout = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        tmp = x
+        x = self.norm1(x)
+        x = self.attention(x)
+        x = self.dropout(x)
+        x = x + tmp
+
+        tmp = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.dropout(x)
+        x = x + tmp
+        return x
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.transformer_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, x):
+        batch_size, seq_len = x.shape
+        tok_emb = self.tok_emb(x)
+        pos_emb = self.pos_emb(torch.arange(seq_len, device=x.device))
+        x = tok_emb + pos_emb
+        x = self.drop_emb(x)
+        x = self.transformer_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+
 torch.manual_seed(123)
-batch_size, context_length, d_in = batch.shape
-d_out = 2
-mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
-context_vecs = mha(batch)
-print(context_vecs)
-print("context_vecs.shape:", context_vecs.shape)
+model = GPTModel(GPT_CONFIG_124M)
+
+out = model(batch)
+print("Input batch:\n", batch)
+print("\nOutput shape:", out.shape)
