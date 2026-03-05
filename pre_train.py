d5406b1772b7c3baff2c4585f367125fdc67cd60
LiuJiajun <ljj@U-2NC6P7LC-0542.local>
2026-03-05 10:11:51 +0800

    Add dataloader functionality for training

--- pre_train.py
+++ pre_train.py
@@ -2,7 +2,9 @@ import torch
 import tiktoken
 from model import GPTModel
 from model import generate_text_simple
+from tokenizer import create_dataloader_v1
 
+torch.manual_seed(123)
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,     # 词汇表大小
@@ -23,7 +25,6 @@ def token_ids_to_text(token_ids, tokenizer):
     return tokenizer.decode(token_ids.squeeze(0).tolist())
 
 
-torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
 model.eval()
 
@@ -36,4 +37,52 @@ token_ids = generate_text_simple(
     max_new_tokens=10,
     context_length=GPT_CONFIG_124M["context_length"]
 )
-print("output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+inputs = torch.tensor([[16833, 3626, 6100],   # ["every effort moves",
+                       [40,    1107, 588]])   #  "I really like"]
+
+targets = torch.tensor([[3626, 6100, 345  ],  # [" effort moves you",
+                        [1107, 588, 11311]])  #  " really like chocolate"]
+
+with torch.no_grad():
+    logits = model(inputs)
+
+logits_flat = logits.flatten(0, 1)
+targets_flat = targets.flatten()
+loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+
+
+file_path = "the-verdict.txt"
+with open(file_path, "r", encoding="utf-8") as file:
+    text_data = file.read()
+
+train_ratio = 0.90
+split_idx = int(train_ratio * len(text_data))
+train_data = text_data[:split_idx]
+val_data = text_data[split_idx:]
+
+train_loader = create_dataloader_v1(
+    train_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=True,
+    shuffle=True,
+    num_workers=0
+)
+val_loader = create_dataloader_v1(
+    val_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0
+)
+print("Train loader:")
+for x, y in train_loader:
+    print(x.shape, y.shape)
+
+print("\nValidation loader:")
+for x, y in val_loader:
+    print(x.shape, y.shape)
--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,9 @@
 import urllib.request
 import re
+import torch
+from torch.utils.data import Dataset, DataLoader
+import tiktoken
+
 
 # 下载文件
 url = ("https://raw.githubusercontent.com/rasbt/"
@@ -11,17 +15,14 @@ urllib.request.urlretrieve(url, file_path)
 # 读取文件内容
 with open(file_path, "r", encoding="utf-8") as f:
     raw_text = f.read()
-print("Total number of character:", len(raw_text))
 
 # 对文本进行预处理, 包括分词和移除空字符串
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
-print(len(preprocessed))
 
 # 构建词汇表, 包括所有唯一的单词
 all_words = sorted(set(preprocessed))
 vocab_size = len(all_words)
-print(vocab_size)
 
 # 构建词汇表, 并将每个单词映射到一个唯一的整数
 vocab = {token:integer for integer,token in enumerate(all_words)}
@@ -42,12 +43,39 @@ class SimpleTokenizerV1:
         text = re.sub(r'\s+([,.:;?_!"()\'])', r'\1', text)
         return text
 
-tokenizer = SimpleTokenizerV1(vocab)
-text = """"It's the last he painted, you know,"
-       Mrs. Gisburn said with pardonable pride."""
-ids = tokenizer.encode(text)
-print(ids)
-print(tokenizer.decode(ids))
 
-text = "Hello, do you like tea?"
-print(tokenizer.encode(text))
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.stride = stride
+        self.input_ids = []
+        self.target_ids = []
+        
+        token_ids = tokenizer.encode(txt)
+        
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1:i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+    
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset, 
+        batch_size=batch_size, 
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+    return dataloader
+
