b1b2b55c0cfffbf11e2825c53dfb724a0db6c43b
LiuJiajun <ljj@U-2NC6P7LC-0542.local>
2026-03-08 19:26:51 +0800

    Add checkpoint loading for inference

--- pre_train.py
+++ pre_train.py
@@ -7,6 +7,8 @@ from model import generate_text_simple, generate
 from tokenizer import create_dataloader_v1
 
 torch.manual_seed(123)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+tokenizer = tiktoken.get_encoding("gpt2")
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,     # 词汇表大小
@@ -123,49 +125,58 @@ def train_model_simple(model, train_loader, val_loader,
 
 
 # 训练数据
-file_path = "the-verdict.txt"
-with open(file_path, "r", encoding="utf-8") as file:
-    text_data = file.read()
+# file_path = "the-verdict.txt"
+# with open(file_path, "r", encoding="utf-8") as file:
+#     text_data = file.read()
 
-train_ratio = 0.90
-split_idx = int(train_ratio * len(text_data))
-train_data = text_data[:split_idx]
-val_data = text_data[split_idx:]
+# train_ratio = 0.90
+# split_idx = int(train_ratio * len(text_data))
+# train_data = text_data[:split_idx]
+# val_data = text_data[split_idx:]
 
-train_loader = create_dataloader_v1(
-    train_data,
-    batch_size=2,
-    max_length=GPT_CONFIG_124M["context_length"],
-    stride=GPT_CONFIG_124M["context_length"],
-    drop_last=True,
-    shuffle=True,
-    num_workers=0
-)
-val_loader = create_dataloader_v1(
-    val_data,
-    batch_size=2,
-    max_length=GPT_CONFIG_124M["context_length"],
-    stride=GPT_CONFIG_124M["context_length"],
-    drop_last=False,
-    shuffle=False,
-    num_workers=0
-)
+# train_loader = create_dataloader_v1(
+#     train_data,
+#     batch_size=2,
+#     max_length=GPT_CONFIG_124M["context_length"],
+#     stride=GPT_CONFIG_124M["context_length"],
+#     drop_last=True,
+#     shuffle=True,
+#     num_workers=0
+# )
+# val_loader = create_dataloader_v1(
+#     val_data,
+#     batch_size=2,
+#     max_length=GPT_CONFIG_124M["context_length"],
+#     stride=GPT_CONFIG_124M["context_length"],
+#     drop_last=False,
+#     shuffle=False,
+#     num_workers=0
+# )
 
+# model = GPTModel(GPT_CONFIG_124M)
+# model.to(device)
+# optimizer = torch.optim.AdamW(
+#     model.parameters(),
+#     lr=0.0004,
+#     weight_decay=0.1
+# )
+# num_epochs = 10
+# train_losses, val_losses, tokens_seen = train_model_simple(
+#     model, train_loader, val_loader, optimizer, device,
+#     num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+#     start_context="Every effort moves you", tokenizer=tokenizer
+# )
+# torch.save({
+#     "model_state_dict": model.state_dict(),
+#     "optimizer_state_dict": optimizer.state_dict(),
+#     },
+#     "model_and_optimizer.pth"
+# )
+checkpoint = torch.load("model_and_optimizer.pth", map_location=device)
 model = GPTModel(GPT_CONFIG_124M)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-optimizer = torch.optim.AdamW(
-    model.parameters(),
-    lr=0.0004,
-    weight_decay=0.1
-)
-num_epochs = 10
-tokenizer = tiktoken.get_encoding("gpt2")
-train_losses, val_losses, tokens_seen = train_model_simple(
-    model, train_loader, val_loader, optimizer, device,
-    num_epochs=num_epochs, eval_freq=5, eval_iter=5,
-    start_context="Every effort moves you", tokenizer=tokenizer
-)
+model.load_state_dict(checkpoint["model_state_dict"])
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-4, weight_decay=0.1)
+optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
 
 # epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
 # plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
